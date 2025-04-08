d74f6f6f3289e5426400ee3e672e1047026be11c
planT-444 <williamhu03@gmail.com>
2025-04-08 10:13:22 -0400

    p_distribution finished?

--- scripts/p_distribution.py
+++ scripts/p_distribution.py
@@ -30,11 +30,20 @@ def num_pairs(drawn_cards: list[int]) -> int:
         rank_occurences[card] = (rank_occurences[card] + 1) % 2
     return total_pairs
 
-distribution = [.0] * (NUM_CARDS + 1)
+distribution = [{} for _ in range (NUM_CARDS + 1)]
 for num_drawn in range(NUM_CARDS + 1):
     trials = (num_pairs(draw(num_drawn)) for i in range(NUM_TRIALS))
-    # lsdkjflkdjfjdfk aaaaaaaaaaaaaaaaaaaaaaaa
+    for pairs in trials:
+        if pairs not in distribution[num_drawn]:
+            distribution[num_drawn][pairs] = 0
+        distribution[num_drawn][pairs] += 1
+    for pairs in distribution[num_drawn]:
+        distribution[num_drawn][pairs] /= NUM_TRIALS
+        
 
 with open(FILENAME, 'w') as f:
-    pass
-    
+    f.write("Probability distributions for each number of cards drawn:\n<# pairs>: <P(# pairs)>\n")
+    for num_drawn, num_drawn_distribution in enumerate(distribution):
+        f.write(f"\nDistribution for {num_drawn} cards drawn:\n")
+        for pairs in sorted(num_drawn_distribution):
+            f.write(f"{pairs}: {num_drawn_distribution[pairs]}\n")
